d239e7032f629c22910dfaad77fa142d55fa4b2a
Aleksandr Bevz <as-bivz@yandex.ru>
2021-03-13 11:25:20 +0300

    Implemented all methods class TestOrderService

--- src/test/java/ru/bevz/DataTestOrderService.java
+++ src/test/java/ru/bevz/DataTestOrderService.java
@@ -2,13 +2,12 @@ package ru.bevz;
 
 import lombok.Data;
 
-import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
 @Data
 public class DataTestOrderService {
 	Map<UUID, Application> storage;
-	List<Object> expectedResult;
-	List<Object> values;
+	Object expectedResult;
+	Object values;
 }
--- src/test/java/ru/bevz/TestOrderService.java
+++ src/test/java/ru/bevz/TestOrderService.java
@@ -14,9 +14,10 @@ import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-//TODO: реализовать все тесты для методов класса OrderService
+//TODO: составить выборку для тестирования
 class TestOrderService {
 	private static final String ERROR_TITLE = "Ошибка";
 	private static final Map<String, List<DataTestOrderService>> data = readDataForTest();
@@ -37,12 +38,8 @@ class TestOrderService {
 		ObjectMapper mapper = new ObjectMapper();
 		mapper.registerModule(new JavaTimeModule());
 		try {
-			//TODO: решить проблему чтения всех файлов,
-			return mapper.readValue(
-							json,
-							new TypeReference<Map<String, List<DataTestOrderService>>>() {
-							}
-			);
+			return mapper.readValue(json, new TypeReference<Map<String, List<DataTestOrderService>>>() {
+			});
 		} catch (IOException e) {
 			JOptionPane.showMessageDialog(
 							null,
@@ -54,39 +51,216 @@ class TestOrderService {
 		return new HashMap<>();
 	}
 
-	private static Stream<Arguments> provideDataForTestFindApplicationsByClientId() {
+	private static Stream<Arguments> provideDataForTestOrderServiceByMethodName(String name) {
 		Stream<Arguments> stream = Stream.of();
-		for (DataTestOrderService data : data.get(Thread.currentThread().getStackTrace()[1].getMethodName())) {
-			stream = Stream.concat(
-							stream,
-							Stream.of(Arguments.of(
-											data.storage,
-											data.expectedResult,
-											data.values.get(0)
-											//TODO: автоматизировать передачу values отдельными значениями аргументов
-							)));
+		for (DataTestOrderService data : data.get(name)) {
+			if (data.values == null) {
+				stream = Stream.concat(
+								stream,
+								Stream.of(Arguments.of(
+												data.storage,
+												data.expectedResult
+								)));
+			} else {
+				stream = Stream.concat(
+								stream,
+								Stream.of(Arguments.of(
+												data.storage,
+												data.expectedResult,
+												data.values
+								)));
+			}
 		}
 		return stream;
 	}
 
+	private static Stream<Arguments> provideDataForTestFindApplicationsByClientId() {
+		return provideDataForTestOrderServiceByMethodName(Thread.currentThread().getStackTrace()[1].getMethodName());
+	}
+
+	private static Stream<Arguments> provideDataForTestFindApplicationsWithoutProducts() {
+		return provideDataForTestOrderServiceByMethodName(Thread.currentThread().getStackTrace()[1].getMethodName());
+	}
+
+	private static Stream<Arguments> provideDataForTestGetActionsByEmployee() {
+		return provideDataForTestOrderServiceByMethodName(Thread.currentThread().getStackTrace()[1].getMethodName());
+	}
+
+	private static Stream<Arguments> provideDataForTestFindApplicationsByActionType() {
+		return provideDataForTestOrderServiceByMethodName(Thread.currentThread().getStackTrace()[1].getMethodName());
+	}
+
+	private static Stream<Arguments> provideDataForTestFindProductsByDescription() {
+		return provideDataForTestOrderServiceByMethodName(Thread.currentThread().getStackTrace()[1].getMethodName());
+	}
+
+	private static Stream<Arguments> provideDataForTestFindClientsByActionAndProducts() {
+		return provideDataForTestOrderServiceByMethodName(Thread.currentThread().getStackTrace()[1].getMethodName());
+	}
+
+	private static Stream<Arguments> provideDataForTestFindNewApplications() {
+		return provideDataForTestOrderServiceByMethodName(Thread.currentThread().getStackTrace()[1].getMethodName());
+	}
+
+	private static Stream<Arguments> provideDataForTestFindClientProducts() {
+		return provideDataForTestOrderServiceByMethodName(Thread.currentThread().getStackTrace()[1].getMethodName());
+	}
+
+	private static Stream<Arguments> provideDataForTestFindEmployeeActions() {
+		return provideDataForTestOrderServiceByMethodName(Thread.currentThread().getStackTrace()[1].getMethodName());
+	}
+
+	private static void replaceStorage(OrderService orderService, Map<UUID, Application> storage) {
+		try {
+			Field fieldStorage = orderService.getClass().getDeclaredField("storage");
+			setAbsolutelyAccessible(fieldStorage);
+			fieldStorage.set(orderService, storage);
+		} catch (Exception e) {
+			// ignored exception
+		}
+	}
+
 	@ParameterizedTest
 	@MethodSource("provideDataForTestFindApplicationsByClientId")
-	void TestFindApplicationsByClientId(
+	void testFindApplicationsByClientId(
 					HashMap<UUID, Application> storage,
 					ArrayList<Object> expected,
-					String clientId
-	) throws Exception {
+					HashMap<String, Object> values
+	) {
 		OrderService orderService = new OrderService();
-		Field fieldStorage = orderService.getClass().getDeclaredField("storage");
+		replaceStorage(orderService, storage);
 
-		setAbsolutelyAccessible(fieldStorage);
-		fieldStorage.set(orderService, storage);
-
-		List<String> listUUID = new ArrayList<>();
-		for (Application app : orderService.findApplicationsByClientId(clientId)) {
-			listUUID.add(app.getId().toString());
-		}
-
+		List<String> listUUID = orderService.findApplicationsByClientId((String) values.get("clientId"))
+						.stream()
+						.map(x -> x.getId().toString())
+						.collect(Collectors.toList());
 		Assertions.assertEquals(expected, listUUID);
 	}
+
+	@ParameterizedTest
+	@MethodSource("provideDataForTestFindApplicationsWithoutProducts")
+	void testFindApplicationsWithoutProducts(
+					HashMap<UUID, Application> storage,
+					ArrayList<Object> expected
+	) {
+		OrderService orderService = new OrderService();
+		replaceStorage(orderService, storage);
+
+		List<String> listApplication = orderService.findApplicationsWithoutProducts()
+						.stream()
+						.map(x -> x.getId().toString())
+						.collect(Collectors.toList());
+		Assertions.assertEquals(expected, listApplication);
+	}
+
+	@ParameterizedTest
+	@MethodSource("provideDataForTestGetActionsByEmployee")
+	void testGetActionsByEmployee(
+					HashMap<UUID, Application> storage,
+					ArrayList<Object> expected,
+					HashMap<String, Object> values
+	) {
+		OrderService orderService = new OrderService();
+		replaceStorage(orderService, storage);
+
+		List<String> listActionId = orderService.getActionsByEmployee((String) values.get("employeeId"))
+						.stream()
+						.map(x -> x.getActionId().toString())
+						.collect(Collectors.toList());
+		Assertions.assertEquals(expected, listActionId);
+	}
+
+	@ParameterizedTest
+	@MethodSource("provideDataForTestFindApplicationsByActionType")
+	void testFindApplicationsByActionType(
+					HashMap<UUID, Application> storage,
+					ArrayList<Object> expected,
+					HashMap<String, Object> values
+	) {
+		OrderService orderService = new OrderService();
+		replaceStorage(orderService, storage);
+
+		List<String> listUUID = orderService.findApplicationsByActionType((ActionType) values.get("actionType"))
+						.stream()
+						.map(x -> x.getId().toString())
+						.collect(Collectors.toList());
+		Assertions.assertEquals(expected, listUUID);
+	}
+
+	@ParameterizedTest
+	@MethodSource("provideDataForTestFindProductsByDescription")
+	void testFindProductsByDescription(
+					HashMap<UUID, Application> storage,
+					ArrayList<Object> expected,
+					HashMap<String, Object> values
+	) {
+		OrderService orderService = new OrderService();
+		replaceStorage(orderService, storage);
+
+		List<String> listProductId = orderService.findProductsByDescription((String) values.get("description"))
+						.stream()
+						.map(Product::getProductId)
+						.collect(Collectors.toList());
+		Assertions.assertEquals(expected, listProductId);
+	}
+
+	@ParameterizedTest
+	@MethodSource("provideDataForTestFindClientsByActionAndProducts")
+	void testFindClientsByActionAndProducts(
+					HashMap<UUID, Application> storage,
+					Set<Object> expected
+	) {
+		OrderService orderService = new OrderService();
+		replaceStorage(orderService, storage);
+
+		Set<String> listClients = orderService.findClientsByActionAndProducts();
+		Assertions.assertEquals(expected, listClients);
+	}
+
+	@ParameterizedTest
+	@MethodSource("provideDataForTestFindNewApplications")
+	void testFindNewApplications(
+					HashMap<UUID, Application> storage,
+					ArrayList<Object> expected
+	) {
+		OrderService orderService = new OrderService();
+		replaceStorage(orderService, storage);
+
+		List<String> listUUID = orderService.findNewApplications()
+						.stream()
+						.map(x -> x.getId().toString())
+						.collect(Collectors.toList());
+		Assertions.assertEquals(expected, listUUID);
+	}
+
+	@ParameterizedTest
+	@MethodSource("provideDataForTestFindClientProducts")
+	void testFindClientProducts(
+					HashMap<UUID, Application> storage,
+					ArrayList<Object> expected,
+					HashMap<String, Object> values
+	) {
+		OrderService orderService = new OrderService();
+		replaceStorage(orderService, storage);
+
+		List<String> listProductId = orderService.findClientProducts((String) values.get("clientId"))
+						.stream()
+						.map(Product::getProductId)
+						.collect(Collectors.toList());
+		Assertions.assertEquals(expected, listProductId);
+	}
+
+	@ParameterizedTest
+	@MethodSource("provideDataForTestFindEmployeeActions")
+	void testFindEmployeeActions(
+					HashMap<UUID, Application> storage,
+					HashMap<String, ActionType> expected
+	) {
+		OrderService orderService = new OrderService();
+		replaceStorage(orderService, storage);
+
+		Map<String, ActionType> mapEmployeeActions = orderService.findEmployeeActions();
+		Assertions.assertEquals(expected, mapEmployeeActions);
+	}
 }
+
